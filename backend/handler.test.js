bc80e9c3825285273d238defe97008e729feabe7
Marian Boucek <boucekm@gmail.com>
2018-11-23 18:13:43 +0000

    Adds Jest to backend

---
+++ backend/handler.test.js
@@ -0,0 +1,20 @@
+const handler = require('./handler')
+
+it('should just work', () => {
+  return new Promise((resolve, reject) => {
+
+    const event = {
+      queryStringParameters: {
+        query: "{greeting(firstName: \"Jeremy\")}"
+      }
+    }
+    const context = {}
+
+    handler.query(event, context, (error, response) => {
+      expect(error).toBeNull()
+      expect(response.body).toBe('{"data":{"greeting":"Hello, Jeremy."}}')
+
+      resolve()
+    })
+  })
+})
