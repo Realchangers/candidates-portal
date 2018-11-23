e2f89cd3b6c0052081c6dec7fe970852ca5c7ed8
Marian Boucek <boucekm@gmail.com>
2018-11-23 18:25:53 +0000

    Adds Jest config to frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "name": "mts-documents-api",
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": -10
+    }
+  }
+}
