f7296c1b6e095ec6989691dd24890bacaebc810c
Marian Boucek <boucekm@gmail.com>
2018-11-26 10:23:17 +0000

    Removes HTTPS from localhost backend invocation

--- frontend/src/RelayEnvironment.js
+++ frontend/src/RelayEnvironment.js
@@ -6,7 +6,7 @@ import {
 } from 'relay-runtime';
 
 function fetchQuery(operation, variables, ) {
-  return fetch('https://localhost:8080/query', {
+  return fetch('http://localhost:8080/query', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
