b57f53316fbdec5969f68c17ab18f255c9f6ea88
Marian Boucek <boucekm@gmail.com>
2018-12-02 19:34:23 +0000

    Adds tests for config.js

---
+++ backend/test/config.test.js
@@ -0,0 +1,17 @@
+const config = require('../src/config')
+
+it('should create AWS DocumentClient correctly for local environment', () => {
+  process.env.IS_OFFLINE = true
+
+  const client = config.documentClient()
+  expect(client).toBeDefined()
+  expect(client.service.endpoint.href).toBe('http://localhost:8000/')
+})
+
+it('should create AWS DocumentClient correctly for production environment', () => {
+  process.env.IS_OFFLINE = false
+
+  const client = config.documentClient()
+  expect(client).toBeDefined()
+  expect(client.service.endpoint.href).toBe('https://dynamodb.eu-west-1.amazonaws.com/')
+})
