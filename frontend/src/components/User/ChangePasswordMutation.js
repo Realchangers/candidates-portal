60a1494133a2b80c75dc4d72f5709c10b75e7a8c
Marian Boucek <boucekm@gmail.com>
2018-12-09 22:03:08 +0000

    Creates changeUserPassword mutation on frontend

---
+++ frontend/src/components/User/ChangePasswordMutation.js
@@ -0,0 +1,35 @@
+import graphql from 'babel-plugin-relay/macro'
+import { commitMutation } from 'react-relay';
+
+const mutation = graphql`
+  mutation ChangePasswordMutation($input: ChangePasswordInput!) {
+    changeUserPassword(input: $input) {
+      user {
+        userName
+        password
+        firstName
+        lastName
+      }
+    }
+  }
+`
+
+function commit(
+  environment,
+  newPassword,
+  user
+) {
+  return commitMutation(
+    environment,
+    {
+      mutation,
+      variables: {
+        currentPassword: user.password,
+        newPassword,
+        userName: user.userName
+      }
+    }
+  )
+}
+
+export default { commit }
