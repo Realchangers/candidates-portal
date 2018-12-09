6baf9bc268644a201f272b317f1be4492dce61b7
Marian Boucek <boucekm@gmail.com>
2018-12-09 15:47:17 +0000

    Creates UserProfile container

---
+++ frontend/src/containers/UserProfile/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import JobOfferList from '../../components/JobOfferList'
+
+import graphql from 'babel-plugin-relay/macro'
+import { QueryRenderer } from 'react-relay';
+
+import environment from '../../RelayEnvironment'
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query UserProfileQuery {
+            user(userName: "test@gmail.com") {
+              userName
+              ...JobOfferList_jobOffers
+            }
+          }
+        `}
+        variables={{}}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Unable to read data. Error: {error.message}</div>
+          }
+
+          if (!props) {
+            return <div>Loading...</div>
+          }
+
+          return (
+            <div>
+              <div>User: {props.user.userName}</div>
+              <JobOfferList jobOffers={props.user} />
+            </div>
+          )
+        }}
+      />
+    );
+  }
+}
+
+export default UserProfile
