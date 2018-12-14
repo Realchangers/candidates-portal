43533ded44488bb2285e481d6033adb894cbbfdd
Marian Boucek <boucekm@gmail.com>
2018-12-15 00:37:49 +0100

    Infrastructure moved to London

--- frontend/infra/create-stack.sh
+++ frontend/infra/create-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws cloudformation create-stack \
   --stack-name Web-React-App \
-  --region eu-west-1 \
+  --region eu-west-2 \
   --template-body file://cloud-formation.yml \
--- frontend/infra/update-stack.sh
+++ frontend/infra/update-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws cloudformation update-stack \
   --stack-name Web-React-App \
-  --region eu-west-1 \
+  --region eu-west-2 \
   --template-body file://cloud-formation.yml \
