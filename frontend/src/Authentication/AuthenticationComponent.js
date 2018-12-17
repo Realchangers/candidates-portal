1ff2c40cf938263bd630f6e0f76aa3b0819b0a5b
Marian Boucek <boucekm@gmail.com>
2018-12-17 15:23:31 +0100

    Adds handlers for AuthenticationComponent

--- frontend/src/Authentication/AuthenticationComponent.js
+++ frontend/src/Authentication/AuthenticationComponent.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class AuthenticationComponent extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
   render() {
     return (
       <form>
@@ -14,7 +27,8 @@ class AuthenticationComponent extends Component {
             <div className="medium-6 cell">
               <label>
                 Email
-                <input type="email" placeholder="user@example.com" />
+                <input type="email" placeholder="user@example.com" name="username"
+                  value={this.state.username} onChange={this.handleInputChange} />
               </label>
             </div>
           </div>
@@ -22,18 +36,39 @@ class AuthenticationComponent extends Component {
             <div className="medium-6 cell">
               <label>
                 Password
-                <input type="password" />
+                <input type="password" name="password"
+                  value={this.state.password} onChange={this.handleInputChange} />
               </label>
             </div>
           </div>
           <div className="grid-x grid-padding-x">
             <div className="medium-6 cell">
-              <button type="submit" className="button success">Login</button>
+              <button type="submit"
+                className="button success"
+                onClick={this.handleLogin}>
+                Login
+              </button>
             </div>
           </div>
         </div>
       </form>
     )
+  }
+
+  handleInputChange(event) {
+    const value = event.target.value
+    const name = event.target.name
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleLogin(event) {
+    event.preventDefault()
+
+    const { username, password } = this.state
+    console.log(`Going to do login with username=${username}, password: ${password}`)
   }
 }
 
