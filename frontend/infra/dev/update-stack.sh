04362ec442cd2b9a9ee38817718ab67ee517c00d
Marian Boucek <boucekm@gmail.com>
2018-12-18 22:42:47 +0100

    Moves CF templates to subfolders

--- frontend/infra/dev/update-stack.sh
+++ frontend/infra/dev/update-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws cloudformation create-stack \
-  --stack-name Candidates-Portal-Frontend \
+aws cloudformation update-stack \
+  --stack-name candidates-portal-frontend-dev \
   --region eu-west-2 \
   --template-body file://cloud-formation.yml \
