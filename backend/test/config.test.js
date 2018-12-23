3a2ac7f1cfc1324e7e7dd2649e2e8de3c447516f
Marian Boucek <boucekm@gmail.com>
2018-12-23 15:53:49 +0100

    Fixes DynamoDB config test

--- backend/test/config.test.js
+++ backend/test/config.test.js
@@ -1,4 +1,5 @@
 const config = require('../src/config')
+const AWS = require('aws-sdk')
 
 it('should create AWS DocumentClient correctly for local environment', () => {
   process.env.IS_OFFLINE = true
@@ -9,7 +10,11 @@ it('should create AWS DocumentClient correctly for local environment', () => {
 })
 
 it('should create AWS DocumentClient correctly for production environment', () => {
+
   process.env.IS_OFFLINE = false
+  AWS.config = {
+    region: 'eu-west-2'
+  }
 
   const client = config.documentClient()
   expect(client).toBeDefined()
