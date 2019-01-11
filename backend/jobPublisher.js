1c1f4cbdce2c52bb8254f66624a99279532de1da
Marian Boucek <boucekm@gmail.com>
2019-01-11 16:22:12 +0000

    PoC for Job publisher job

--- backend/jobPublisher.js
+++ backend/jobPublisher.js
@@ -2,8 +2,30 @@
 exports.handler = (event, _context, callback) => {
 
   event.Records.forEach((record) => {
-    console.log(`Event: ${record.eventName}, Record: ${JSON.stringify(record.dynamodb, null, 2)}`)
+    const { NewImage, OldImage, Keys } = record.dynamodb
+
+    const oldLocation = getProfileLocation(OldImage)
+    const newLocation = getProfileLocation(NewImage)
+
+    const identity = Keys.id.S
+    if (oldLocation === newLocation) {
+      console.log(`Identity '${identity}' - no change in location.`)
+    }
+    else {
+      console.log(`Identity '${identity}' - location changed from '${oldLocation}' to '${newLocation}'.`)
+    }
   })
 
   callback(null, `Successfully processed ${event.Records.length} records.`)
+}
+
+function getProfileLocation(record) {
+  if (record.profile) {
+    if (record.profile.M) {
+      if (record.profile.M.location) {
+        return record.profile.M.location.S
+      }
+    }
+  }
+  return null
 }
