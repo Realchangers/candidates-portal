5a02addbf675d47e86b7773f33dbbea6d207e650
Marian Boucek <boucekm@gmail.com>
2019-01-16 21:04:39 +0000

    Improves logging in JobPublisher

--- backend/jobPublisher.js
+++ backend/jobPublisher.js
@@ -3,11 +3,17 @@ exports.handler = (event, _context, callback) => {
 
   event.Records.forEach((record) => {
     const { NewImage, OldImage, Keys } = record.dynamodb
+    const identity = Keys.cognitoID.S
+    const recordID = Keys.id.S
+
+    if (recordID !== 'profile') {
+      console.log(`Identity='${identity}' - skipping update for non-profile record. `)
+      return
+    }
 
     const oldLocation = getProfileLocation(OldImage)
     const newLocation = getProfileLocation(NewImage)
 
-    const identity = Keys.id.S
     if (oldLocation === newLocation) {
       console.log(`Identity='${identity}' - field 'location' has not changed`)
     }
